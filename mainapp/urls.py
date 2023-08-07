aa8b2d8109637779543b4f9a8faf3f5c9e184576
lautariano <lautariano777@gmail.com>
2023-08-08 00:08:08 +0500

    Release

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,11 @@
-from .views import MainPageView, CategoryView
+from .views import MainPageView, CategoryView, ServiceView, FAQView, ArticlesView, ArticleView
 from django.urls import path
 
 urlpatterns = [
     path("", MainPageView.as_view(), name="main_page"),
-    path("category/<int:id>", CategoryView.as_view(), name="category_view")
-
+    path("category/<int:id>", CategoryView.as_view(), name="category_view"),
+    path("service/<int:id>", ServiceView.as_view(), name="service_view"),
+    path("faq", FAQView.as_view(), name="faq"),
+    path("articles/", ArticlesView.as_view(), name="articles"),
+    path("article/<int:id>", ArticleView.as_view(), name="article")
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -16,3 +16,46 @@ class CategoryView(View):
         category = Category.objects.get(id=kwargs.get("id"))
         services = Service.objects.filter(category=category)
         return render(request, "mainapp/category.html", {"services": services, "categories": categories, "category": category})
+
+class ServiceView(View):
+
+    def get(self, request, *args, **kwargs):
+        service = Service.objects.get(id=kwargs.get("id"))
+        context = {"service": service}
+        rec_services = Service.objects.all()
+        recommended = []
+        for rec_service in rec_services:
+            if rec_service != service:
+                recommended.append(rec_service)
+        if recommended:
+            context["rec_services"] = recommended[0]
+        else:
+            context["rec_services"] = None
+        return render(request, "mainapp/service.html", context)
+
+class FAQView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "mainapp/faq.html", {})
+
+class ArticlesView(View):
+
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()
+        return render(request, "mainapp/articles.html", {"articles": articles})
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.get(id=kwargs.get("id"))
+        context = {"article": article}
+        recommended = []
+        articles = Article.objects.all()
+        for rec_article in articles:
+            if article != rec_article:
+                recommended.append(rec_article)
+        if recommended:
+            context["rec_article"] = recommended[0]
+        else:
+            context["rec_article"] = None
+        return render(request, "mainapp/article.html", context)
